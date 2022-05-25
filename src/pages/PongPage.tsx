f384da6c5c3556c24ab5b4680525df527a86df3e
Jordan Landry <jordanlandry1999@gmail.com>
2022-05-25 03:38:51 -0400

    Updated size of pong iframe

--- src/pages/PongPage.tsx
+++ src/pages/PongPage.tsx
@@ -6,13 +6,13 @@ type Props = {}
 
 export default function PongPage({}: Props) {
   return (
-    <div>
+    <div className="pong-page">
       <BackButton />
       <iframe 
         className="pong--game" 
         src="https://jordanlandry.github.io/pong/" 
         width={window.innerWidth * 0.8}
-        height={window.innerWidth * 0.8 * 0.45 + 20}   // Source game height = 45vw, adding margin and borders is another 20 or so pixels
+        height={window.innerWidth * 0.4}
         frameBorder={0}
       />
     </div>
